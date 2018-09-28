dcb43a75b3fb27322f74ef940fd80fba44750567
Douglas Johnston <douglas@Holos-MacBook-Pro-6.local>
2018-09-28 14:19:47 -0700

    imports

---
+++ testForTravis.ino
@@ -0,0 +1,18 @@
+#include "AZ3166WiFi.h"
+#include "AzureIotHub.h"
+#include "DevKitMQTTClient.h"
+#include <AZ3166WiFiUdp.h>
+#include <assert.h>
+#include "parson.h"
+#include <Wire.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
